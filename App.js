061952427f263d0c0ccbd5bd32caf54b0bef3eef
Owen Glahn <owenglahn@Owens-MacBook-Air.local>
2021-06-14 21:45:30 -0400

    Button with alert

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, SafeAreaView, Button, Alert } from "react-native";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createStackNavigator } from "@react-navigation/stack";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles}>
+      <Button
+        title="Click Me"
+        onPress={() => {
+          Alert.alert("Clicked", "You clicked on the button 😱", [
+            {
+              text: "Cancel",
+              style: "cancel",
+              onPress: console.log("Cancel Pressed"),
+            },
+            { text: "OK", onPress: console.log("OK pressed") },
+          ]);
+        }}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: 100,
+    height: 100,
   },
 });
---
+++ src/split.js
@@ -0,0 +1,4 @@
+// cost per person as string
+function split(num_people, cost) {
+  return (cost / num_people).toFixed();
+}
